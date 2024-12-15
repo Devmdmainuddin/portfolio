1ddae7b00b624100d37fdb65bef50965e8c0bf94
Devmdmainuddin <devmdmainuddin@gmail.com>
2024-12-15 10:19:44 +0600

    create project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,15 +1,33 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import {dirname} from "path";
+import {fileURLToPath} from "url";
+import {FlatCompat} from "@eslint/eslintrc";
 
+// Resolve the __dirname and __filename for ES module
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Initialize FlatCompat for compatibility with older ESLint configs
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Define the ESLint configuration
 const eslintConfig = [
+  {
+    ignores: [
+      ".next/*",
+      "*.css",
+      "dist",
+      "esm/*",
+      "public/*",
+      "tests/*",
+      "scripts/*",
+      "*.config.js",
+      "node_modules",
+      "coverage",
+      "build",
+    ],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
